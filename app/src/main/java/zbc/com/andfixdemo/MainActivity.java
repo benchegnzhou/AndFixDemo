39941ef1a787ffaafcc4bd497c624b6eac96c2d7
benchegnzhou <1107472286@qq.com>
2019-03-07 19:12:30 +0800

    第一个版本提交补充

---
+++ app/src/main/java/zbc/com/andfixdemo/MainActivity.java
@@ -0,0 +1,22 @@
+package zbc.com.andfixdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * Created by benchengzhou on 2019/3/7  18:07 .
+ * 作者邮箱： mappstore@163.com
+ * 功能描述：
+ * 类    名： MainActivity
+ * 备    注：
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
